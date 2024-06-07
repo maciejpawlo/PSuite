74c198e5ed6814eb1e3b92ac87f91e3ecad91ef1
Maciej Pawłowski <maciejpawlo@gmail.com>
2024-06-07 09:43:39 +0200

    WIP: added Dockerfile for yarp reverse proxy, added test configuration for reverse proxy

--- src/Bootstrapper/PSuite.Bootstrapper/Program.cs
+++ src/Bootstrapper/PSuite.Bootstrapper/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/test-yarp", () => "Routing works!");
 
 app.Run();
