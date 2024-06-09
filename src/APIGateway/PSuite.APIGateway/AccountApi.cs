f19f1abea43e9f40e9c034e345184ddf0fdf575a
Maciej Pawłowski <maciejpawlo@gmail.com>
2024-06-09 22:02:59 +0200

    WIP: added oidc integration in api gateway, added redis session store

---
+++ src/APIGateway/PSuite.APIGateway/AccountApi.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace PSuite.APIGateway;
+
+public static class AccountApi
+{
+    public static RouteGroupBuilder MapAccountApi(this RouteGroupBuilder groups)
+    {
+        groups.MapGet("/public", () => "Welcome to API Gateway").WithName("Public");
+        groups.MapGet("/logout", async (HttpContext context) => 
+        {
+            var prop = new AuthenticationProperties
+            {
+                RedirectUri = "/account/public"
+            };
+
+            if(context.Request.Cookies.Count > 0)
+            {
+                var siteCookies = context.Request.Cookies.Where(c => c.Key.Contains(".AspNetCore.") || c.Key.Contains("Microsoft.Authentication"));
+                foreach (var cookie in siteCookies)
+                {
+                    context.Response.Cookies.Delete(cookie.Key);
+                }
+            }
+            await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, prop);
+            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        })
+        .WithName("Logout")
+        .RequireAuthorization();
+
+        return groups;
+    }
+}
--- src/APIGateway/PSuite.APIGateway/Program.cs
+++ src/APIGateway/PSuite.APIGateway/Program.cs
@@ -1,6 +1,36 @@
+using PSuite.APIGateway;
+using PSuite.APIGateway.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddReverseProxy(builder.Configuration);
+builder.Services.AddOAuthProxy();
+builder.Services.AddAuthorizationPolicies();
+builder.Services.AddCache(builder.Configuration);
+
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapReverseProxy();
+app.MapGroup("account")
+    .MapAccountApi()
+    .WithTags("Account")
+    .WithOpenApi()
+    .WithMetadata();
+
 app.Run();
