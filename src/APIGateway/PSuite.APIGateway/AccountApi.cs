4ce6c8ba0aa11cbd24c5309ec110327ceecb9b1b
Maciej Pawłowski <maciejpawlo@gmail.com>
2024-06-09 22:18:13 +0200

    WIP: fixes in AccountApi so the logout endpoint supports OpenApi doc generation

--- src/APIGateway/PSuite.APIGateway/AccountApi.cs
+++ src/APIGateway/PSuite.APIGateway/AccountApi.cs
@@ -8,28 +8,31 @@ public static class AccountApi
 {
     public static RouteGroupBuilder MapAccountApi(this RouteGroupBuilder groups)
     {
-        groups.MapGet("/public", () => "Welcome to API Gateway").WithName("Public");
-        groups.MapGet("/logout", async (HttpContext context) => 
-        {
-            var prop = new AuthenticationProperties
-            {
-                RedirectUri = "/account/public"
-            };
+        groups.MapGet("/public", () => "Welcome to API Gateway")
+            .WithName("Public");
 
-            if(context.Request.Cookies.Count > 0)
-            {
-                var siteCookies = context.Request.Cookies.Where(c => c.Key.Contains(".AspNetCore.") || c.Key.Contains("Microsoft.Authentication"));
-                foreach (var cookie in siteCookies)
-                {
-                    context.Response.Cookies.Delete(cookie.Key);
-                }
-            }
-            await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, prop);
-            await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        })
-        .WithName("Logout")
-        .RequireAuthorization();
+        groups.MapGet("/logout", (Delegate) HandleLogout)
+            .WithName("Logout")
+            .RequireAuthorization();
 
         return groups;
+    }
+    private static async Task HandleLogout(HttpContext context)
+    {
+        var prop = new AuthenticationProperties
+        {
+            RedirectUri = "/account/public"
+        };
+
+        if(context.Request.Cookies.Count > 0)
+        {
+            var siteCookies = context.Request.Cookies.Where(c => c.Key.Contains(".AspNetCore.") || c.Key.Contains("Microsoft.Authentication"));
+            foreach (var cookie in siteCookies)
+            {
+                context.Response.Cookies.Delete(cookie.Key);
+            }
+        }
+        await context.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme, prop);
+        await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
 }
