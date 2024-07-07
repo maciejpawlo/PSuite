668dbad52a855c8f3b85e5f4f49dee568b52056e
Maciej Pawłowski <maciejpawlo@gmail.com>
2024-07-07 16:15:17 +0200

    WIP: added postgres support

--- src/Shared/PSuite.Infrastructure/Database/Extensions.cs
+++ src/Shared/PSuite.Infrastructure/Database/Extensions.cs
@@ -1,6 +1,17 @@
-﻿namespace PSuite.Shared.Infrastructure.Database;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
-public static class Extensions
+[assembly: InternalsVisibleTo("PSuite.Bootstrapper")]
+namespace PSuite.Shared.Infrastructure.Database;
+internal static class Extensions
 {
-
+    public static IServiceCollection AddPostgres<T>(this IServiceCollection services, PostgresOptions postgresOptions) where T : DbContext
+    {
+        services.AddDbContext<T>(options => 
+        {
+            options.UseNpgsql(postgresOptions.ConnectionString);
+        });
+        return services;
+    }
 }
---
+++ src/Shared/PSuite.Infrastructure/Database/PostgresOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace PSuite.Shared.Infrastructure.Database;
+
+internal class PostgresOptions
+{
+    public const string SectionName = "postgres";
+    public string ConnectionString { get; set; } = string.Empty;
+}
