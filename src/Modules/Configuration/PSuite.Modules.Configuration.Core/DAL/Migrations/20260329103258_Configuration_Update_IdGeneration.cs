8c14e78617a56c2eb4524032eaed7da5c6004c28
Maciej Pawłowski <maciejpawlo@gmail.com>
2026-03-29 12:34:24 +0200

    fix: update snapshot, fix scalar jwt auth

---
+++ src/Modules/Configuration/PSuite.Modules.Configuration.Core/DAL/Migrations/20260329103258_Configuration_Update_IdGeneration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PSuite.Modules.Configuration.Core.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Configuration_Update_IdGeneration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- src/Shared/PSuite.Infrastructure/Authentication/BearerSecuritySchemeTransformer.cs
+++ src/Shared/PSuite.Infrastructure/Authentication/BearerSecuritySchemeTransformer.cs
@@ -8,7 +8,8 @@ internal sealed class BearerSecuritySchemeTransformer : IOpenApiDocumentTransfor
     public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
     {
         document.Components ??= new OpenApiComponents();
-        document.Components.SecuritySchemes?["Bearer"] = new OpenApiSecurityScheme
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+        document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
         {
             Type = SecuritySchemeType.Http,
             Scheme = "bearer",
